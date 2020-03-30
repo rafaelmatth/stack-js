a91558797919fa754e9fd85e406cf6b754eaf923
rafael <rafaelmat142@gmail.com>
2020-03-30 18:04:40 -0300

    feat: mobile create

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Detail(){
     return(
-        <View />
+        <View>
+        <Text>Olá Mundo</Text>
+        </View>
     );
 }
