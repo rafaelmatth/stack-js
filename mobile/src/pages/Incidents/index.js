ce626bfa2a68dd15bab1c83ae2513d459f595e6a
rafael <rafaelmat142@gmail.com>
2020-03-30 03:02:34 -0300

    feat: routes mobile

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import LogoImg from '../../assets/logo.png';
+
 export default function Incidents(){
     return(
         <View />
